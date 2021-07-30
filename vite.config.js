47539234476ae9ea1b7f9847dcb29c43f248a95b
Kunsarifan <64526047+cengir@users.noreply.github.com>
2021-07-30 21:42:01 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,16 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-// module.exportsdefineConfig = {
-//   publicPath: process.env.NODE_ENV === 'production'
+module.exportsdefineConfig = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/portofolio/'
+//     : '/',
+  plugins: [vue()],
+}
+
+// export default defineConfig({
+//    publicPath: process.env.NODE_ENV === 'production'
 //     ? '/portofolio/'
 //     : '/',
-//   plugins: [vue()],
-// }
-
-export default defineConfig({
-   publicPath: process.env.NODE_ENV === 'production'
-    ? '/portofolio/'
-    : '/',
-    plugins: [vue()],
-});
+//     plugins: [vue()],
+// });
